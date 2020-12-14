2c58cf84a30027d0ce47a620f0c99a6bad4f27b4
Riikka Nousiainen <rhnousiainen@gmail.com>
2020-12-14 15:41:11 +0200

    Day 11, faster solution

--- 2020/python/day11/part2.py
+++ 2020/python/day11/part2.py
@@ -1,35 +1,44 @@
 import os
 
-
-def fill_seats(seat_map):
-    filled_seats = [[c for c in row] for row in seat_map.copy()]
-    for i, seat_row in enumerate(seat_map):
-        for j, seat in enumerate(seat_row):
-            if is_seat(seat):
-                visible_seats = get_visible_seats((i, j), seat_map)
-                if is_vacant_seat(seat) and count_occupied_seats(visible_seats) == 0:
-                    filled_seats[i][j] = '#'
-                elif not is_vacant_seat(seat) and count_occupied_seats(visible_seats) >= 5:
-
-                    filled_seats[i][j] = 'L'
-
-    return [''.join(seats) for seats in filled_seats]
+SEAT_STATES = {'vacant': 'L', 'occupied': '#'}
+FLOOR = '.'
 
 
-def is_seat(seat):
-    return True if seat in ('L', '#') else False
+class Seat():
+    def __init__(self, state, position, seat_position_map):
+        self.state = state
+        self.row = position[0]
+        self.col = position[1]
+        self.visible_seat_positions = get_visible_seats(
+            position, seat_position_map)
+
+    def update_state(self, seat_map):
+        visible_seats = [seat_map[i][j]
+                         for i, j in self.visible_seat_positions]
+        if is_vacant_seat(self.state) and count_occupied_seats(visible_seats) == 0:
+            self.state = SEAT_STATES['occupied']
+        elif not is_vacant_seat(self.state) and count_occupied_seats(visible_seats) >= 5:
+            self.state = SEAT_STATES['vacant']
 
 
-def get_visible_seats(current_seat_index, seat_map):
-    row, col = current_seat_index
+def is_vacant_seat(seat):
+    return True if seat == SEAT_STATES['vacant'] else False
 
-    seats_left_to_right = seat_map[row]
-    seats_front_to_back = ''.join([row[col] for row in seat_map])
+
+def count_occupied_seats(seat_map):
+    return [seat for seat_row in seat_map for seat in seat_row].count(SEAT_STATES['occupied'])
+
+
+def get_visible_seats(position, seat_position_map):
+    row, col = position
+
+    seats_left_to_right = seat_position_map[row]
+    seats_front_to_back = [row[col] for row in seat_position_map]
 
     seats_diagonally_left_to_right = get_diagonal_seats(
-        seat_map, current_seat_index)
+        seat_position_map, position)
     seats_diagonally_right_to_left = get_diagonal_seats(
-        seat_map, current_seat_index, right_to_left=True)
+        seat_position_map, position, right_to_left=True)
 
     pos_on_diagonal = row
     if row - col > 0:
@@ -65,10 +74,10 @@ def get_diagonal_seats(matrix, position, right_to_left=False):
     offset = row - col
     if right_to_left:
         offset = - row - col
-        return ''.join([
-            row[-i-offset] for i, row in enumerate(matrix) if 0 <= -i-offset < len(row)])
-    return ''.join([
-        row[i+offset] for i, row in enumerate(matrix) if 0 <= i+offset < len(row)])
+        return [
+            row[-i-offset] for i, row in enumerate(matrix) if 0 <= -i-offset < len(row)]
+    return [
+        row[i+offset] for i, row in enumerate(matrix) if 0 <= i+offset < len(row)]
 
 
 def find_first_seat(seats, start_at, reverse=False):
@@ -79,22 +88,47 @@ def find_first_seat(seats, start_at, reverse=False):
             seats_to_check = seats[::-1]
 
         for seat in seats_to_check[start_at+1:]:
-            if seat == '#' or seat == 'L':
+            if seat != FLOOR:
                 return seat
     return None
 
 
-def is_vacant_seat(seat):
-    return True if seat == 'L' else False
+def initialize_seats(seat_map):
+    seats = []
+    # Replace seats with their position as other seats vacancy is not important at init stage
+    position_map = [[c for c in row] for row in seat_map]
+    for i, seat_row in enumerate(seat_map):
+        for j, seat_state in enumerate(seat_row):
+            if is_seat(seat_state):
+                position_map[i][j] = (i, j)
+
+    # Initialize seat objects
+    for i, seat_row in enumerate(seat_map):
+        for j, seat_state in enumerate(seat_row):
+            if is_seat(seat_state):
+                seats.append(Seat(seat_state, (i, j), position_map))
+
+    return seats
 
 
-def count_occupied_seats(seat_map):
-    return [seat for seat_row in seat_map for seat in seat_row].count('#')
+def is_seat(seat_state):
+    return True if seat_state in SEAT_STATES.values() else False
+
+
+def fill_seats(seats, seat_map):
+    new_seat_map = [[c for c in row] for row in seat_map]
+    for seat in seats:
+        seat.update_state(seat_map)
+        new_seat_map[seat.row][seat.col] = seat.state
+
+    return [''.join(item) for item in new_seat_map]
 
 
 def main():
     with open('{0}/input.txt'.format(os.path.dirname(os.path.realpath(__file__)))) as f:
         seat_map = f.read().splitlines()
+
+    seats = initialize_seats(seat_map)
 
     current_seat_map = seat_map.copy()
     previous_seat_map = []
@@ -102,7 +136,7 @@ def main():
 
     while ''.join(current_seat_map) != ''.join(previous_seat_map):
         previous_seat_map = current_seat_map.copy()
-        current_seat_map = fill_seats(previous_seat_map)
+        current_seat_map = fill_seats(seats, previous_seat_map)
         count += 1
 
     print('Part 2 answer: ', count_occupied_seats(current_seat_map))
--- 2020/python/day11/test_day11_part2.py
+++ 2020/python/day11/test_day11_part2.py
@@ -1,91 +1,108 @@
 import part2
+import unittest
 
 
-def test_get_visible_seats_on_left():
-    seats = ['L.#.L.']
-    assert part2.get_visible_seats((0, 4), seats) == ['#']
+class TestGetVisibleSeats(unittest.TestCase):
+
+    def test_get_visible_seats_on_left(self):
+        seats = ['A.B.X.']
+        self.assertEqual(part2.get_visible_seats((0, 4), seats), ['B'])
+
+    def test_get_visible_seats_on_right(self):
+        seats = ['...X.A']
+        self.assertEqual(part2.get_visible_seats((0, 3), seats), ['A'])
+
+    def test_get_visible_seats_at_front(self):
+        seats = ['.A.', '.X.', '...', '...']
+        self.assertEqual(part2.get_visible_seats((1, 1), seats), ['A'])
+
+    def test_get_visible_seats_at_back(self):
+        seats = ['...', '...', '.X.', '.A.']
+        self.assertEqual(part2.get_visible_seats((2, 1), seats), ['A'])
+
+    def test_get_visible_seats_diagonally_front_left(self):
+        seats = ['A.B', 'C..', '.X.', '...']
+        self.assertEqual(part2.get_visible_seats((2, 1), seats), ['C'])
+
+    def test_get_visible_seats_diagonally_front_right(self):
+        seats = ['A.B', '..C', '.X.', '...']
+        self.assertEqual(part2.get_visible_seats((2, 1), seats), ['C'])
+
+    def test_get_visible_seats_diagonally_back_left(self):
+        seats = ['...', '...', '.X.', 'A..']
+        self.assertEqual(part2.get_visible_seats((2, 1), seats), ['A'])
+
+    def test_get_visible_seats_diagonally_back_right(self):
+        seats = ['...', '...', '.X.', '..A']
+        self.assertEqual(part2.get_visible_seats((2, 1), seats), ['A'])
+
+    def test_get_visible_seats_diagonally_right_corner(self):
+        seats = ['..A..', '..B.C', '..D.X', '....E']
+        self.assertCountEqual(
+            part2.get_visible_seats((2, 4), seats),
+            ['A', 'C', 'D', 'E']
+        )
+
+    def test_get_visible_seats_diagonally_left_corner(self):
+        seats = ['A.B..', 'X...C', 'D...E', '..F..']
+        self.assertCountEqual(
+            part2.get_visible_seats((1, 0), seats),
+            ['A', 'C', 'D', 'F']
+        )
+
+    def test_get_visible_seats_multiple_horizontal(self):
+        seats = ['..A.', '....', 'BX.C', '...D']
+        self.assertCountEqual(
+            part2.get_visible_seats((2, 1), seats),
+            ['B', 'C']
+        )
+
+    def test_get_visible_seats_multiple_vertical(self):
+        seats = ['.AB.', '.C..', '.X..', '.D.E']
+        self.assertCountEqual(
+            part2.get_visible_seats((2, 1), seats),
+            ['C', 'D']
+        )
+
+    def test_get_visible_seats_multiple_diagonal(self):
+        seats = ['...', 'A.B', '.X.', 'C.D']
+        self.assertCountEqual(
+            part2.get_visible_seats((2, 1), seats),
+            ['A', 'B', 'C', 'D']
+        )
+
+    def test_get_visible_seats_multiple(self):
+        seats = ['.A.B', 'C...', 'DXE.', '.F.G']
+        self.assertCountEqual(
+            part2.get_visible_seats((2, 1), seats),
+            ['A', 'B', 'C', 'D', 'E', 'F']
+        )
 
 
-def test_get_visible_seats_on_right():
-    seats = ['...L.#']
-    assert part2.get_visible_seats((0, 3), seats) == ['#']
+class TestGetDiagonalSeats(unittest.TestCase):
 
+    def test_get_diagonal_seats_left_to_right(self):
+        matrix = ['1234', 'abcd', '5678', 'ABCD']
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (0, 0), False), ['1', 'b', '7', 'D'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (2, 2), False), ['1', 'b', '7', 'D'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (1, 2), False), ['2', 'c', '8'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (0, 3), False), ['4'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (2, 0), False), ['5', 'B'])
 
-def test_get_visible_seats_at_front():
-    seats = ['.#.', '.L.', '...', '...']
-    assert part2.get_visible_seats((1, 1), seats) == ['#']
+    def test_get_diagonal_seats_right_to_left(self):
+        matrix = ['1234', 'abcd', '5678', 'ABCD']
 
-
-def test_get_visible_seats_at_back():
-    seats = ['...', '...', '.L.', '.#.']
-    assert part2.get_visible_seats((2, 1), seats) == ['#']
-
-
-def test_get_visible_seats_diagonally_front_left():
-    seats = ['L.L', '#..', '.L.', '...']
-    assert part2.get_visible_seats((2, 1), seats) == ['#']
-
-
-def test_get_visible_seats_diagonally_front_right():
-    seats = ['L.L', '..#', '.L.', '...']
-    assert part2.get_visible_seats((2, 1), seats) == ['#']
-
-
-def test_get_visible_seats_diagonally_back_left():
-    seats = ['...', '...', '.L.', '#..']
-    assert part2.get_visible_seats((2, 1), seats) == ['#']
-
-
-def test_get_visible_seats_diagonally_back_right():
-    seats = ['...', '...', '.L.', '..#']
-    assert part2.get_visible_seats((2, 1), seats) == ['#']
-
-
-def test_get_visible_seats_diagonally_right_corner():
-    seats = ['..#..', '..L.#', '..#.L', '....#']
-    assert part2.get_visible_seats((2, 4), seats) == ['#', '#', '#', '#']
-
-
-def test_get_visible_seats_diagonally_left_corner():
-    seats = ['#.L..', 'L...#', '#...L', '..#..']
-    assert part2.get_visible_seats((1, 0), seats) == ['#', '#', '#', '#']
-
-
-def test_get_visible_seats_multiple_horizontal():
-    seats = ['..L.', '....', '#L.#', '...L']
-    assert part2.get_visible_seats((2, 1), seats) == ['#', '#']
-
-
-def test_get_visible_seats_multiple_vertical():
-    seats = ['.LL.', '.#..', '.L..', '.#.L']
-    assert part2.get_visible_seats((2, 1), seats) == ['#', '#']
-
-
-def test_get_visible_seats_multiple_diagonal():
-    seats = ['...', '#.#', '.L.', '#.#']
-    assert part2.get_visible_seats((2, 1), seats) == ['#', '#', '#', '#']
-
-
-def test_get_visible_seats_multiple():
-    seats = ['.#.#', '#...', '#L#.', '.#.L']
-    assert part2.get_visible_seats((2, 1), seats) == [
-        '#', '#', '#', '#', '#', '#']
-
-
-def test_get_diagonal_seats_left_to_right():
-    matrix = ['1234', 'abcd', '5678', 'ABCD']
-    assert part2.get_diagonal_seats(matrix, (0, 0), False) == '1b7D'
-    assert part2.get_diagonal_seats(matrix, (2, 2), False) == '1b7D'
-    assert part2.get_diagonal_seats(matrix, (1, 2), False) == '2c8'
-    assert part2.get_diagonal_seats(matrix, (0, 3), False) == '4'
-    assert part2.get_diagonal_seats(matrix, (2, 0), False) == '5B'
-
-
-def test_get_diagonal_seats_right_to_left():
-    matrix = ['1234', 'abcd', '5678', 'ABCD']
-
-    assert part2.get_diagonal_seats(matrix, (0, 0), True) == '1'
-    assert part2.get_diagonal_seats(matrix, (2, 2), True) == 'd7B'
-    assert part2.get_diagonal_seats(matrix, (1, 2), True) == '4c6A'
-    assert part2.get_diagonal_seats(matrix, (0, 3), True) == '4c6A'
-    assert part2.get_diagonal_seats(matrix, (2, 0), True) == '3b5'
+        self.assertEqual(part2.get_diagonal_seats(matrix, (0, 0), True), ['1'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (2, 2), True), ['d', '7', 'B'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (1, 2), True), ['4', 'c', '6', 'A'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (0, 3), True), ['4', 'c', '6', 'A'])
+        self.assertEqual(part2.get_diagonal_seats(
+            matrix, (2, 0), True), ['3', 'b', '5'])
