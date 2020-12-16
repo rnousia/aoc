0eea10ccb5cafbead083e493333d0471df92dcf7
Riikka Nousiainen <rnousia@users.noreply.github.com>
2020-12-16 19:15:17 +0200

    Refactor day 15, part 2

--- 2020/python/day15/part2.py
+++ 2020/python/day15/part2.py
@@ -2,53 +2,44 @@ class NumberGame():
     def __init__(self, starting_numbers):
         self.numbers = {}
         for i, number in enumerate(starting_numbers):
-            self.add_number(number, i)
-
-    def add_number(self, value, index):
+            self.numbers[number] = NumberToPlay(number, i)
+        self.current_number_value = starting_numbers[-1]
+        
+    def play_next_number(self, index):
+        value = self.numbers[self.current_number_value].current_index - \
+            self.numbers[self.current_number_value].last_index
         if value not in self.numbers:
             self.numbers[value] = NumberToPlay(value, index)
         else:
             self.numbers[value].update(index)
-
-        self.current_number = self.numbers[value]
-        self.current_index = index
-
-    def play_next_number(self):
-        self.current_index += 1
-        if self.current_number.count == 1:
-            self.add_number(0, self.current_index)
-        elif self.current_number.count > 1:
-            self.add_number(self.current_number.index_step, self.current_index)
-
+        self.current_number_value = value
 
 class NumberToPlay():
     def __init__(self, number, index):
         self.value = number
-        self.count = 1
         self.current_index = index
-        self.index_step = None
+        self.last_index = index
 
     def update(self, new_index):
-        self.count += 1
-        self.index_step = new_index - self.current_index
+        self.last_index = self.current_index
         self.current_index = new_index
 
     def __str__(self):
-        return 'Value: {0}, count:{1}, current index:{2}'.format(self.value, self.count, self.current_index)
+        return 'Value: {0}, current index:{2}'.format(self.value, self.current_index)
 
 
 def main():
     # input
     starting_numbers = [16, 1, 0, 18, 12, 14, 19]
+    # test input
     # starting_numbers = [0, 3, 6]
 
     game = NumberGame(starting_numbers)
 
-    for i in range(len(starting_numbers), 2020):
-        # print(i)
-        game.play_next_number()
+    for i in range(len(starting_numbers), 30000000):
+        game.play_next_number(i)
 
-    print('Part 2 answer: ', game.current_number.value)
+    print('Part 2 answer: ', game.current_number_value)
 
 
 if __name__ == '__main__':
